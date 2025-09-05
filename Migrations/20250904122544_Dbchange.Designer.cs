9775897c31b9f91e2a5acc7fc1a9f49e5339b914
abastein <marko.kolenko@port.si>
2025-09-05 12:02:14 +0200

    v3

--- Migrations/20250904122544_Dbchange.Designer.cs
+++ Migrations/20250904122544_Dbchange.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LM01_UI.Data.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LM01_UI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250904122544_Dbchange")]
+    partial class Dbchange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
